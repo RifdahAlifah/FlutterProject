edbab42cb5b1e7b750d97ae5dc791099f5a82f7f
RifdahAlifah <rifdahalifah37@gmail.com>
2021-03-02 10:32:16 +0700

    Flutter Project

---
+++ FlutterProject/portal_berita/android/app/src/main/kotlin/com/idn/rifdahalf/portal_berita/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.idn.rifdahalf.portal_berita
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ FlutterProject/portal_berita/lib/service/repositories.dart
@@ -0,0 +1,4 @@
+class NewsRepositories{
+  static String baseUrl = 'http://newsapi.org/v2/';
+  static String API_KEY = 'df94a5d76b064303845138e3c42c8dd6';
+}
